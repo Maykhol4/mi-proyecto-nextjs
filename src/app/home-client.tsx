b8e5daf3180d11cb5011da060c94aa5db58a1630
Diosdado Maykhol Jurado Mulato <maykholplayer4@gmail.com>
2025-09-14 23:23:26 +0000

    TypeError: onConnectionStateChanged is not a function     at BleConnecto

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -163,7 +163,7 @@ export default function HomeClient() {
     <>
       <BleConnector
         ref={bleConnectorRef}
-        onConnectionStateChange={setConnectionState}
+        onConnectionStateChanged={setConnectionState}
       />
 
       <WifiConfigModal 
